1a578cc8971e64cce9c476cdac5ca022a5ed49db
logonrm <logonrm@fiap.com.br>
2017-10-09 21:57:22 -0300

    Novo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
+/*Módulos */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RoutingModule } from './routing/routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+/* Serviços */
+import { DespesaService } from './despesa.service';
+
+
+/*Componentes */
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
+
+/* LocalStorage */
+import { LocalStorageModule } from 'angular-2-local-storage';
+import { DecimalBrPipe } from './decimal-br.pipe';
+import {MomentModule} from 'angular2-moment';
+import 'moment/locale/pt-br';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    AddComponent,
+    DecimalBrPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RoutingModule,
+    NgbModule.forRoot(),
+    LocalStorageModule.withConfig({
+      prefix: 'despesasApp',
+      storageType: 'localStorage'
+    }),
+    MomentModule
+
   ],
-  providers: [],
+  providers: [DespesaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
